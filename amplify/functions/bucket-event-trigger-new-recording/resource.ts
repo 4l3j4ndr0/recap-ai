b6d9590265de0e3aaad63f27e6593c7d1430363e
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2025-09-06 12:07:51 -0500

    :rocket: Chhanges timeot lambda function onNewRecording

--- amplify/functions/bucket-event-trigger-new-recording/resource.ts
+++ amplify/functions/bucket-event-trigger-new-recording/resource.ts
@@ -3,7 +3,7 @@ import { defineFunction } from "@aws-amplify/backend";
 export const OnNewRecordFunction = defineFunction({
   name: "bucket-event-trigger-new-recording",
   runtime: 22,
-  timeoutSeconds: 30,
+  timeoutSeconds: 300,
   memoryMB: 2048,
   ephemeralStorageSizeMB: 1024,
 });
